91a68edfaa99cc1864781b47d6ae9c91c5390e87
monseeligio <71795808+monseeligio@users.noreply.github.com>
2020-10-30 18:03:27 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+package examenu2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class ExamenU2 {
+
+    public static void main(String[] args) {
+              proceso obj =new proceso();
+
+                obj.par(0,0);
+                obj.mostrar();
+
+    }
+}
