45ac23acc5ef6c9e1b0c415223f80e5627b5b1a0
monseeligio <71795808+monseeligio@users.noreply.github.com>
2020-10-30 18:03:09 -0600

    Create proceso.java

---
+++ proceso.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenu2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author gama
+ */
+public class proceso {
+    
+          List lista = new ArrayList();
+          List lista2 = new ArrayList();
+
+    public int par(int contpar, int contimpar){
+     Scanner lector = new Scanner(System.in);      
+      if (contpar<5 && contimpar<5){
+         System.out.println("inserte numero");
+         int num;
+         num= lector.nextInt();
+         if (num%2==0){
+          lista.add(num);
+         }
+         
+         else{
+         if (contimpar<5){
+          lista2.add(num);
+                 }
+                 }
+         
+      par(contpar+1, contimpar+1);
+      }
+      return contpar;
+        }
+    
+  
+    
+    public void mostrar(){
+      for (int i = 0; i <= lista.size() - 1; i++) {
+            System.out.println(i+"."+lista.get(i));
+}  
+    
+    } 
